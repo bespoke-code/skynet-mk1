a9ba1805c7abe40e2dd4c6990e67748202db367b
Andrej <andrej.zgeorgievski@gmail.com>
2014-04-13 15:40:32 +0200

    result.java

---
+++ result.java
@@ -0,0 +1,16 @@
+
+public class result {
+/*
+ * RESULT:
+ * a class which takes care of the output files
+ * that the telescopes would have to return to the server.
+ * Here, we can utilize the power of JAVA to write 
+ * methods to send pictures and text over a network.
+ * 
+ * Other users could build on this platform. As 'result' is a 
+ * proper class, different networks may write additional code 
+ * to send different data types over a network.
+ * 
+ * The concept stays the same in all cases.
+ */
+}
